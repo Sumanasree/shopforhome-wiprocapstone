bf39be358b5541211f93437eb7bb0ae9643b2d7d
Sumanasree <sumanasreeala@gmail.com>
2022-08-24 22:16:57 +0530

    frontend

---
+++ frontend/src/app/models/product-info-main.ts
@@ -0,0 +1,13 @@
+export class ProductInfoMain {
+
+    productId: string;
+    productName: string;
+    productPrice: number;
+    productStock: number;
+    productDescription: string;
+    productIcon: string;
+    productStatus: number;
+    categoryType: number;
+    createTime: string;
+    updateTime: string;
+}
